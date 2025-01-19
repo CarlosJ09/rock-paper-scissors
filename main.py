f3b42d1da8e233c69d6867c0467ad28578fd58f3
CarlosJ09 <c.jimenez@dominicanadeseguros.com>
2025-01-19 01:54:27 -0400

    init project

---
+++ main.py
@@ -0,0 +1,16 @@
+class Paper():
+    def __init__(self):
+        pass
+
+class Rock():
+    def __init__(self):
+       pass
+
+class Scissors():
+    def __init__(self):
+       pass
+
+def main():
+    print("Hello")
+
+main()
